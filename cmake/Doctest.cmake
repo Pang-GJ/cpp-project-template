4f9d59b18770ec3152d674c1cc7e5fc019f0def2
Pang-GJ <2362421192@qq.com>
2022-08-11 10:36:46 +0800

    create a complex template

---
+++ cmake/Doctest.cmake
@@ -0,0 +1,12 @@
+if(ENABLE_DOCTESTS)
+    add_definitions(-DENABLE_DOCTEST_IN_LIBRARY)
+    include(FetchContent)
+    FetchContent_Declare(
+            DocTest
+            GIT_REPOSITORY "https://github.com/doctest/doctest"
+            GIT_TAG "v2.4.9"
+    )
+
+    FetchContent_MakeAvailable(DocTest)
+    include_directories(${DOCTEST_INCLUDE_DIR})
+endif()
